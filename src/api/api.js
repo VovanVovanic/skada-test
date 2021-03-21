30fd43793c6c07a34d8b6af8849661e32640220a
Vladimir <vladimirplotnikov371@gmail.com>
2021-03-22 01:54:38 +0200

    cors

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
 export const getPosts = (count, limit=4) => {
-  return axios.get(`http://jsonplaceholder.typicode.com/posts?_start=${count}&_limit=${limit}`)
+  return axios.get(`http://jsonplaceholder.typicode.com/posts?_start=${count}&_limit=${limit}`,
+  {withCredentials: true})
     .then((res)=>res.data)
 }
 export const getData = (id) => {
-  return axios.get(`http://jsonplaceholder.typicode.com/posts/${id}/comments?_start=2&_limit=2`)
+  return axios.get(`http://jsonplaceholder.typicode.com/posts/${id}/comments?_start=2&_limit=2`,
+  {withCredentials:true})
     .then((res)=>res.data)
 }
 export const addData = (body) => {
